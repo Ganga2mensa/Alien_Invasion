b198c2b08faa6c2f605bc77b509f649bd970e480
Iamsolomon <finot2006@gmail.com>
2024-05-09 22:09:21 +0300

    Displaying the Level.

--- game_stats.py
+++ game_stats.py
@@ -11,3 +11,4 @@ class GameStat:
         """Initialize statistics that can change during the game."""
         self.ships_left = self.settings.ship_limit
         self.score = 0
+        self.level = 1
