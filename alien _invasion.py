35faebdaa21a04bbc8b032c0d478a5ef5f97b4c6
Iamsolomon <finot2006@gmail.com>
2024-05-09 15:02:32 +0300

    Starting the Game.

--- alien _invasion.py
+++ alien _invasion.py
@@ -71,6 +71,9 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
 
     def _check_keydown_events(self, event):
         """Respond to keypresses."""
@@ -201,6 +204,11 @@ class AlienInvasion:
                 # Treat this the same as if the ship got hit.
                 self._ship_hit()
                 break
+    
+    def _check_play_button(self, mouse_pos):
+        """Start a new game when the player clicks Play."""
+        if self.play_button.rect.collidepoint(mouse_pos):
+            self.game_active = True
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
